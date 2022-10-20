e3fd71cd178a90d684f58524e5b372d1a7d61472
frknztrkk <105914984+frknztrkk@users.noreply.github.com>
2022-10-20 22:35:38 +0300

    Automationexercise sitesi web otomasyon testi
    automationexercise sitesine girip üyelik oluşturup,ürün ekleyip ,  sepete ekleyip, adres bilgileri girip ödeme sayfasına yönlendirilmesini kontrol eden otomasyon tesi

---
+++ com.automationexercise.selenium/src/test/java/BaseTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeAll
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.get("https://www.automationexercise.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+    }
+
+
+    @AfterAll
+    public void tearDown(){
+        driver.quit();
+    }
+   
+}
